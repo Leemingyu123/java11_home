359d068f46c2181226db3a6f7db8a3d618b32da3
Leemingyu123 <alsrb7776@gmail.com>
2024-02-21 17:27:45 +0900

    java 11 condition

--- text04/src/org/kh/app/Condition4.java
+++ text04/src/org/kh/app/Condition4.java
@@ -27,10 +27,125 @@ public class Condition4 {
 		else if(point>=67) {result = "D+";} else if(point>=63) {result = "D0";} else if (point>=60) {result = "D-";}
 		else {result = "F";}
 		
-	
+		/*
+		if(point>=90) {
+			result = "A";
+			if((point%10>=7 && point%10<=9) || point==100) {
+				result = result + "+";
+			} else if(point%10>=3 && point%10<=6) {
+				result = result + "0";
+			} else {
+				result = result + "-";
+			}
+		} else if(point>=80) {
+			result = "B";
+			if(point%10>=7 && point%10<=9) {
+				result = result + "+";
+			} else if(point%10>=3 && point%10<=6) {
+				result = result + "0";
+			} else {
+				result = result + "-";
+			}
+		} else if(point>=70) {
+			result = "C";
+			if(point%10>=7 && point%10<=9) {
+				result = result + "+";
+			} else if(point%10>=3 && point%10<=6) {
+				result = result + "0";
+			} else {
+				result = result + "-";
+			}
+		} else if(point>=60) {
+			result = "D";
+			if(point%10>=7 && point%10<=9) {
+				result = result + "+";
+			} else if(point%10>=3 && point%10<=6) {
+				result = result + "0";
+			} else {
+				result = result + "-";
+			}
+		} else {
+			result = "F";
+		}
+		*/
+/*
+		if((point%10>=7 && point%10<=9 && point>=60) || point==100) {
+			result = "+";
+			if(point>=90) {
+				result = "A"+result;
+			} else if(point>=80) {
+				result = "B"+result;
+			} else if(point>=70) {
+				result = "C"+result;
+			} else {
+				result = "D"+result;
+			}	
+		} else if(point%10>=3 && point%10<=6 && point>=60) {
+			result = "0";
+			if(point>=90) {
+				result = "A"+result;
+			} else if(point>=80) {
+				result = "B"+result;
+			} else if(point>=70) {
+				result = "C"+result;
+			} else {
+				result = "D"+result;
+			}	
+		} else if(point%10>=0 && point%10<=2 && point>=60) {
+			result = "-";
+			if(point>=90) {
+				result = "A"+result;
+			} else if(point>=80) {
+				result = "B"+result;
+			} else if(point>=70) {
+				result = "C"+result;
+			} else {
+				result = "D"+result;
+			}
+		} else {
+			result = "F";
+		}
+
+		//switch~case~ : 
+		/*
+		switch(비교대상){
+			case 값1:
+				실행문;
+				break;
+			case 값2:
+				실행문;
+				break;
+			.....	
+			case 값n:
+				실행문n;
+				break;
+			default:
+				실행문;
+		} 
+		*/
+		String remark = "";
+		switch(result){
+			case "A+":
+				remark = "학업우수상";
+				break;
+			case "A0":
+				remark = "노력상";
+				break;
+			case "A-":
+				remark = "아차상";
+				break;
+			default:
+				remark = "";
+		} 
+		System.out.println("당신의 점수 : "+point);
+		System.out.println("당신의 학점 : "+result);
+		System.out.println("당신의 시상내역 : "+remark);
+		
 		
 		System.out.printf("\n당신의 점수는 %d 입니다. ", point);
 		System.out.println("\n당신의 학점은 "+result);
+		
+		
 		
 
 
