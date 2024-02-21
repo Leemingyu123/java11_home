919d1900314f998b79be9d291ed9cadbc5a7ad24
Leemingyu123 <alsrb7776@gmail.com>
2024-02-21 14:10:51 +0900

    java 11 installation & variable & operation

---
+++ test01/src/org/kh/java/Hello.java
@@ -0,0 +1,13 @@
+package org.kh.java;
+
+public class Hello {
+	public static void main(String[] args) {	//실행을 위한 기본 main 메소드=ctrl+/ 한 줄 각주
+		System.out.println("Hello~! \n Java~!");
+		/*
+		 작 ㅣ이름 소개하는 출력 명력
+		 결과 : My nave is mingyu= crtl+shift+/ 여러 줄 각주
+		 */
+		//sysout = ctrl+spacebar = 단축명령 자동 완성(Code Hinting)
+		System.out.println("My nave is mingyu");
+	}
+}
